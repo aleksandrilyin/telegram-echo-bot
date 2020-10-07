dfa5d268569f55238947a8688c4bcdc490b3f2b9
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-10-07 22:08:06 +0300

    Add a no warranty clause for a program

--- bot.rb
+++ bot.rb
@@ -1,3 +1,13 @@
+# Telegram Echo Bot
+
+# Copyright © 2020 Alexander Ilyin <alexander.ilyin11@gmail.com>
+
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://www.wtfpl.net/ for more details.
+
 require 'telegram/bot'
 
 token = ENV['TELEGRAM_BOT_API_TOKEN']
